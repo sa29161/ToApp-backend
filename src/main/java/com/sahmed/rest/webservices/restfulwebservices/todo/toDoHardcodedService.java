977c28b2079f810ff044af0d381ac7bda0e7522e
sa29161 <sa1489053@gmail.com>
2020-12-20 14:31:18 -0500

    added security to web app

--- src/main/java/com/sahmed/rest/webservices/restfulwebservices/todo/toDoHardcodedService.java
+++ src/main/java/com/sahmed/rest/webservices/restfulwebservices/todo/toDoHardcodedService.java
@@ -22,6 +22,19 @@ public class toDoHardcodedService {
 		return todos;
 	}
 	
+	public Todo save(Todo todo) {
+		if(todo.getId() == -1 || todo.getId() == 0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}
+		else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		
+		return todo;
+	}
+	
 	public Todo deleteById(long id) {
 		Todo todo = findById(id);
 		if(todo == null) return null;
@@ -31,7 +44,7 @@ public class toDoHardcodedService {
 		return null;
 	}
 
-	private Todo findById(long id) {
+	public Todo findById(long id) {
 		for (Todo todo : todos) {
 			if (todo.getId() == id) {
 				return todo;
